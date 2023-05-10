7bbe24c630e4d46d37aa7bc92bb74bf55eefa3d1
Clayton <persinger.clayton@gmail.com>
2023-05-10 12:22:36 -0700

    Setup initial client side connection to snake server.

---
+++ play.js
@@ -0,0 +1,22 @@
+const net = require('net');
+
+//establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: '50541',
+  });
+
+  //Interpret incoming data
+  conn.setEncoding('utf-8');
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+console.log("connecting...");
+connect();
+
