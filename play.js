8c790b8a5b6009d5f4221daae45743e6bc0e54d9
Clayton <persinger.clayton@gmail.com>
2023-05-10 12:28:38 -0700

    Moved client side code to a seperate file called client.js and imported necessary functions to play.js.

--- play.js
+++ play.js
@@ -1,22 +1,4 @@
-const net = require('net');
-
-//establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: '50541',
-  });
-
-  //Interpret incoming data
-  conn.setEncoding('utf-8');
-
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
+const { connect } = require('./client');
 
 console.log("connecting...");
 connect();
-
