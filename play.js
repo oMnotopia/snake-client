c196efcc9e5c8de97ac047118ade105f04387e5e
Clayton <persinger.clayton@gmail.com>
2023-05-10 12:54:09 -0700

    Updated play.js to use stdin that contains setup code and an event listener for keypresses. On the event of ctr-c being pressed close the connection.

--- play.js
+++ play.js
@@ -2,3 +2,24 @@ const { connect } = require('./client');
 
 console.log("connecting...");
 connect();
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    console.log("Thanks for using me, ciao!");
+    process.exit();
+  }
+};
+
+//Setup interface to handle user input from stdin
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+setupInput();
